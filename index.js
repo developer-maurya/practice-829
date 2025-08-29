cec481a6c9b20778b23668e1e0e85c6eaf0ccf0a
Chitranjan kumar <chitranjan75075@gmail.com>
2025-08-29 21:34:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+// let arr = [ 1,2,3,4,5]
+// console.log(arr.length);
+
+// push
+
+// const arr1 = ["hello", "india"]
+// let res = arr1.push("& world")
+// console.log(arr1)
+
+// slice
+// let arr2 = ["hello", "india" ,"world"]
+// let res1 = arr1.slice(1, 2)
+// console.log(arr1)
+// console.log(res)
+
+// toString
+// const arr3 = ["hello", "india"]
+// let res2 = arr3.toString()
+// console.log(res2);
+
+// shift
+
+// const arr4 = ["hello", "india"]
+// let res3 = arr1.shift()
+// let res3 = arr1.unshift()
+// console.log(res3)
+// console.log(arr1)
+
+// Map
+// const arr5 = [1,2,3,4,5]
+// let res4 = arr5.map((a)=> a + 2)
+// console.log(res4)
+
+// filter
+// const arr6 = ["hello","india","word", "hii"]
+// let res5 = arr6.filter(word => word.length >5)
+// console.log(res5);
+
+// const arr7 = [1,2,3,4,5]
+// let res6 = arr5.find((a)=> a==2)
+// console.log(res6)
+
+// const arr8 = [1,2,3,4,5]
+// let res7 = arr8.findIndex((a)=> a==4)
+// console.log(res7)
+
+// const arr9 = [1,2,3,4,5]
+// let res8 = arr9.reverse()
+// console.log(res8)
+
+// const arr10 = [1,2,3,4,5]
+// let res9 = delete(arr[3])
+// console.log(arr10);
+
+
+// let arr =[1,2,3,4,5,6]
+// let res2= arr.map((y) => y*3);
+
+// console.log(res2);
+
+// push
+// const arr1 = ["chitranjan","kumar" ,"maurya" , "singh"]
+// console.log(arr1)
+//  let res1 = arr1.filter( word => word.length > 3)
+// console.log(res1)
+
+let arr4 =[1,2,3,4,5,6,7]
+let ches = arr4.find(x => x == 2)
+let ches1 = arr4.findIndex(x => x ==2)
+let ches2 = arr4.includes(5)
+let ches3 =arr4.reverse()
+let ches4 = arr4.slice(3,4,10)
+let ches5= arr4.unshift(9)
+console.log(ches1);
+console.log(ches);
+console.log(ches2);
+console.log(ches3);
+console.log(ches4);
+console.log(ches5)
+console.log(arr4);
+
+
+
+
+
